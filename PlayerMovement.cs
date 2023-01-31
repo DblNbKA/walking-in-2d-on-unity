783b750240d944852414ae88f0e05055d201f96a
DblNbKA <104922491+DblNbKA@users.noreply.github.com>
2023-01-31 13:40:06 +0100

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 10f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = Vector2.zero;
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            rb.velocity += Vector2.left * Speed;
+        }
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            rb.velocity += Vector2.right * Speed;
+        }
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            rb.velocity += Vector2.up * Speed;
+            }
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            rb.velocity += Vector2.down * Speed;
+        }
+    }
+}
